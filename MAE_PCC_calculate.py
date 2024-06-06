87f5cdb5f57e20c4e2eeaa2698a7b1f61942f9a4
WZH5127 <74030901+WZH5127@users.noreply.github.com>
2024-06-06 15:08:32 +0800

    Add files via upload

--- MAE_PCC_calculate.py
+++ MAE_PCC_calculate.py
@@ -26,7 +26,6 @@ class AspectBasedSentimentModel(nn.Module):
         return valence_predict, arousal_predict
 
 
-# 加载数据
 def load_data_from_file(file_path, train=True):
     data = []
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -47,10 +46,11 @@ def load_data_from_file(file_path, train=True):
 
 
 def calculate_metrics(preds, labels):
+    preds = torch.tensor(preds).squeeze().numpy()
+    labels = torch.tensor(labels).squeeze().numpy()
     mae = mean_absolute_error(labels, preds)
     pcc, _ = pearsonr(labels, preds)
     return mae, pcc
-
 
 def get_predictions(model, dataloader):
     model.eval()
@@ -75,40 +75,22 @@ def get_predictions(model, dataloader):
     return all_valence_preds, all_arousal_preds, all_valence_labels, all_arousal_labels
 
 
-# 设置全局随机种子
 random_seed = 0
 set_seed(random_seed)
-# 准备数据
-print('加载数据')
-all_data = load_data_from_file('data/train1.txt', train=True)
+
+all_data = load_data_from_file('data/train2.txt', train=True)
 train_data, eval_data = train_test_split(all_data, test_size=0.1, random_state=random_seed, shuffle=True)
 
 train_dataloader = DataLoader(train_data, batch_size=32)
 eval_dataloader = DataLoader(eval_data, batch_size=32)
 
-# 加载最佳模型
-print('加载最佳模型')
 best_model = torch.load('./model/best_chinese-bert-wwm-ext2.pth')
 
-# 获取训练集和验证集上的预测值和真实值
-print('计算训练集上的预测值和真实值')
-train_valence_preds, train_arousal_preds, train_valence_labels, train_arousal_labels = (
-    get_predictions(best_model, train_dataloader))
-
-print('计算验证集上的预测值和真实值')
 eval_valence_preds, eval_arousal_preds, eval_valence_labels, eval_arousal_labels = (
     get_predictions(best_model, eval_dataloader))
 
-# 计算训练集上的 MAE 和 PCC
-train_valence_mae, train_valence_pcc = calculate_metrics(train_valence_preds, train_valence_labels)
-train_arousal_mae, train_arousal_pcc = calculate_metrics(train_arousal_preds, train_arousal_labels)
-
-# 计算验证集上的 MAE 和 PCC
 eval_valence_mae, eval_valence_pcc = calculate_metrics(eval_valence_preds, eval_valence_labels)
 eval_arousal_mae, eval_arousal_pcc = calculate_metrics(eval_arousal_preds, eval_arousal_labels)
 
-print(f'训练集 Valence MAE: {train_valence_mae:.4f}, Valence PCC: {train_valence_pcc:.4f}')
-print(f'训练集 Arousal MAE: {train_arousal_mae:.4f}, Arousal PCC: {train_arousal_pcc:.4f}')
-
-print(f'验证集 Valence MAE: {eval_valence_mae:.4f}, Valence PCC: {eval_valence_pcc:.4f}')
-print(f'验证集 Arousal MAE: {eval_arousal_mae:.4f}, Arousal PCC: {eval_arousal_pcc:.4f}')
+print(f'Valence MAE: {eval_valence_mae:.4f}, Valence PCC: {eval_valence_pcc:.4f}')
+print(f'Arousal MAE: {eval_arousal_mae:.4f}, Arousal PCC: {eval_arousal_pcc:.4f}')
